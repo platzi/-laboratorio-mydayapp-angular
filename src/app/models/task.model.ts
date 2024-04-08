1d18f419d96f633b8897a6fa908042ce39baeee1
Alfredo20164920 <apaz20164920@gmail.com>
2024-04-07 20:15:15 -0600

    Update task & change states
    - Update tasks
    - Change styles when change the mode (completed/editing)
    - Valid that the edit task is in a valid format

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,5 +1,6 @@
 export interface Task {
     id: string,
     title: string, 
-    completed: boolean
+    completed: boolean,
+    editing?: boolean
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, Injector, OnChanges, OnInit, SimpleChanges, effect, inject, signal } from '@angular/core';
-import { TasksService } from '../../services/tasks.service';
 import { Task } from '../../models/task.model';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -12,7 +11,6 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-  private tasksServcie = inject(TasksService);
   tasks = signal<Task[]>([]);
 
   titleControl = new FormControl("", {
@@ -35,16 +33,16 @@ export class HomeComponent implements OnInit {
     this.trackTasks()
   }
 
-  trackTasks() {
+  trackTasks(): void {
     effect(() => {
       const tasks = this.tasks();
       localStorage.setItem("mydayapp-angular", JSON.stringify(tasks));
     }, {injector: this.injector})
   }
 
-  add(): void {
+  addTask(): void {
     if(this.titleControl.valid) {
-      const title =  this.titleControl.value?.trim() || "";
+      const title =  this.formatTask(this.titleControl.value)
       if(title !== "") {
         this.setTask(title);
         this.titleControl.setValue("")
@@ -60,6 +58,82 @@ export class HomeComponent implements OnInit {
     }
 
     this.tasks.update((prev) => [newTask, ...prev]);
+  }
+
+  toggleCompleted(index: number): void {
+    this.tasks.update( (tasks: Task[]) => {
+      return tasks.map((item, i) => {
+        if(index == i) {
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        }
+
+        return item
+      })
+    })
+  }
+
+  editingMode(index: number): void {
+    if(this.tasks()[index].completed) return;
+
+    this.tasks.update( (tasks: Task[]) => {
+      return tasks.map((item, i) => {
+        if(index == i) {
+          return {
+            ...item,
+            editing: true,
+          }
+        }
+
+        return {
+          ...item,
+          editing: false
+        }
+      })
+    })
+  }
+
+  updateTask(e: Event, index: number): void {
+    const inputElement = e.target as HTMLInputElement;
+    const newTitle = this.formatTask(inputElement.value);
+
+    if(newTitle !== "") {
+      this.tasks.update( (tasks: Task[]) => {
+        return tasks.map((item, i) => {
+          if(index == i) {
+            return {
+              ...item,
+              editing: false,
+              title: newTitle
+            }
+          }
+  
+          return item
+        })
+      })
+    }
+
+  }
+
+  formatTask(title: string | null): string {
+    return title?.trim() || "";
+  }
+
+  exitEditingMode() {
+    this.tasks.update( (tasks: Task[]) => {
+      return tasks.map((item, i) => {
+        return {
+          ...item,
+          editing: false
+        }
+      })
+    })
+  }
+
+  exam() {
+    console.log(1);
   }
 
 }
