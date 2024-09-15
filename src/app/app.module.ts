64ff5081de8272812b021080f793093c79bc6dcc
Jorge Medina <jlsilva@ectotec.com>
2024-09-15 08:22:17 -0600

    feat(): Generate basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './domains/shared/components/layout/layout.component';
+import { FooterComponent } from './domains/todo/components/footer/footer.component';
+import { HeaderComponent } from './domains/todo/components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LayoutComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
