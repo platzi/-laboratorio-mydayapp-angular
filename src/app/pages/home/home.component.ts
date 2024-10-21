fc0eb5ea300e6c5cd5e0bf37ea10f930bef46b76
Nicolas-Guasch <nic.guasch@gmail.com>
2024-10-21 10:45:50 -0300

    Add task creation and display

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,4 +9,18 @@ export class HomeComponent {
   tasksList = signal<appTask[]>([]);
 
   constructor() {}
+
+  addTask(event: Event) {
+    const inputBox = event.target as HTMLInputElement;
+    const taskTitle = inputBox.value.trim();
+    inputBox.value = '';
+    if (taskTitle != '') {
+      const newTask: appTask = {
+        id: Date.now().toString(),
+        title: taskTitle,
+        completed: false,
+      };
+      this.tasksList.update((list) => [...list, newTask]);
+    }
+  }
 }
