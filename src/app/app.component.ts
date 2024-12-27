adb64d0aa9586d2f5e9bdb9d64f397c26ae3f8f6
Luis Quijada <luis.quijada@sonorasoft.com>
2024-12-26 20:01:24 -0700

    changed config files for v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet></router-outlet>',
-  standalone: true
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {}
