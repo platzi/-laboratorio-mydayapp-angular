e31ac054a0ef88bf113efddc4927a547e9af2b7d
Luis Quijada <luis.quijada@sonorasoft.com>
2024-12-26 19:03:31 -0700

    changes for version 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    template: '<router-outlet></router-outlet>',
-    standalone: false
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: '<router-outlet></router-outlet>',
+  standalone: true
 })
 export class AppComponent {}
